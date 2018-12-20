e33ff1eae349ac082f96c26eb2903957c7649289
Will Darnell <will@willdarnell.net>
2018-12-19 21:41:34 -0800

    stub out interrupt handler

--- wdMail-Transmitter.ino
+++ wdMail-Transmitter.ino
@@ -2,12 +2,29 @@
 // Auth: Will Darnell
 // Date: 19-DEC-2018
 
-void setup() {
-  // put your setup code here, to run once:
+const int  sensorPin      =  2; // Number of pin to listen to
+const long debouncingTime = 15; // Milliseconds to wait for debounce
 
+volatile int  sensorState = 0;
+volatile long lastMicros;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  attachInterrupt(0, debounceInterrupt, CHANGE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void debounceInterrupt() {
+  if ((long)(micros() - lastMicros) >= debouncingTime * 1000) {
+    onSensorChange();
+    lastMicros = micros();
+  }
+}
+
+void onSensorChange() {
+   
 }
