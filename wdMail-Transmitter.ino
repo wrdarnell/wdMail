57f4d2955753900ce47971fad310553761e2370c
Will Darnell <will@willdarnell.net>
2018-12-19 21:58:59 -0800

    limit duplicate alerts

--- wdMail-Transmitter.ino
+++ wdMail-Transmitter.ino
@@ -2,11 +2,13 @@
 // Auth: Will Darnell
 // Date: 19-DEC-2018
 
-const int  sensorPin    =   2; // Number of pin to listen to
-const long debouncingMs = 250; // Milliseconds to wait for debounce
+const int  sensorPin     =   2; // Number of pin to listen to
+const long debouncingMs  = 250; // Milliseconds to wait for debounce
+const int  dupAlertWaitS =   1; // Seconds to wait until firing another alert
 
 volatile          int  sensorState = 0;
 volatile unsigned long lastSensorFireMs;
+volatile unsigned long lastAlertMs;
 
 void setup() {
   pinMode(sensorPin, INPUT);
@@ -26,5 +28,12 @@ void debounceInterrupt() {
 }
 
 void onSensorChange() {
-   
+   if (((long)millis() - lastAlertMs) >= (dupAlertWaitS * 1000)) {
+      sendAlert();
+      lastAlertMs = millis(); 
+   }
+}
+
+void sendAlert() {
+  
 }
