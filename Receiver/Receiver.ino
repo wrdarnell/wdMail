b858fe97636bb685935b2cb7ee9002cbb7574c4d
Will Darnell <will@willdarnell.net>
2018-12-19 23:15:04 -0800

    add receiver

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,15 @@
+// File: Receiver.ino
+// Auth: Will Darnell
+// Date: 19-DEC-2018
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "../../wdMail/Common/wdMail.h"
+
+void setup() {
+  
+}
+
+void loop() {
+}
