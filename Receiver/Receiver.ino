ecb0f40a62e1eb81e03c6a131c9620f8b602fc68
Will Darnell <will@willdarnell.net>
2018-12-19 23:23:37 -0800

    stub out test code

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -7,9 +7,24 @@
 #include <RF24.h>
 #include "../../wdMail/Common/wdMail.h"
 
+// TEST
+const int ledPin = LED_BUILTIN;
+// END TEST
+
 void setup() {
-  
+  // Test Code
+  pinMode(ledPin, OUTPUT); // Setup the on-board LED
+  // End Test Code
 }
 
 void loop() {
+}
+
+void HelloLED() {
+  int i;
+  for (i=0;i<5;i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+  }
 }
